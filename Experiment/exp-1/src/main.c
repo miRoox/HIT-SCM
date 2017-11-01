77c2619d5bdc92ed2b07bcb2d132eb33ac744d9b
miRoox <miroox@outlook.com>
2017-11-01 15:07:41 +0800

    improve

--- Experiment/exp-1/src/main.c
+++ Experiment/exp-1/src/main.c
@@ -8,6 +8,8 @@ typedef unsigned short uint16;
 
 void delay(void);
 
+#ifndef IMPROVE
+
 int main(void)
 {
     LED = 0;
@@ -21,6 +23,39 @@ int main(void)
 
     return 0;
 }
+
+#else // 实验的改进
+
+volatile bit stopped;
+
+int main(void)
+{
+    LED = 0;
+    stopped = 0;
+
+    EX0=1;  // 允许外部INT0中断
+    IT0=1;  // INT0边沿触发
+    EA=1;   // 允许中断
+
+    forever
+    {
+        if(!stopped)
+        {
+            if(LED==0) LED = 0xff; // 共阳，低电平有效
+            LED <<= 1; // 从上到下
+            delay();
+        }
+    }
+
+    return 0;
+}
+
+void onPress(void) interrupt 0 using 0
+{
+    stopped = ~stopped;
+}
+
+#endif
 
 void delay(void)
 {
