cf85e0d83167198dc81615e3177ba2ce6afdc580
miRoox <miroox@outlook.com>
2017-11-07 23:56:05 +0800

    fix item 8

--- Homework/Item8/src/main.c
+++ Homework/Item8/src/main.c
@@ -11,24 +11,24 @@ void delay(void);
 int main(void)
 {
     LED = 0; // 共阳，低电平有效
-    
+
     EX0=1;  // 允许外部INT0中断
     IT0=1;  // INT0边沿触发
     EA=1;   // 允许中断
-    
+
     forever
     {
         LED = 0;
     }
-    
+
     return 0;
 }
 
 void onTrigger(void) interrupt 0 // INT0
 {
-    LED = 0xaa; //0b10101010
+    LED = 0xf0;
     delay();
-    LED = 0x55; //0b01010101
+    LED = 0x0f;
     delay();
 }
 
