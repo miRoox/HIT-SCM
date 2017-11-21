4ebc9772712a30051665203085c6ee7420fbe625
miRoox <miroox@outlook.com>
2017-11-21 16:44:01 +0800

    experiment 15

---
+++ Experiment/Exp15/main.c
@@ -0,0 +1,37 @@
+#include <REGX51.H>
+
+#define DAOut P1
+sbit DACS = P2^0;
+sbit DAWS1 = P2^1;
+
+#define forever for(;;)
+
+typedef unsigned char uint8;
+
+void delay(void);
+
+int main(void)
+{
+    uint8 i = 0;
+
+    DACS = 0;  // 片选
+    DAWS1 = 0; // 一直写入
+
+    forever
+    {
+        DAOut = i++;
+        delay();
+    }
+
+    return 0;
+}
+
+void delay(void)
+{
+    volatile uint8 i = 0xff;
+    do {
+        volatile uint8 j =0xff;
+        while(--j);
+    }
+    while(--i);
+}
